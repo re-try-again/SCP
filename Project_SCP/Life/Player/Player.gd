01ae83773bc5bc94d9268da39f1a369b71bdbe28
Retry <revinb.rumbarb@gmail.com>
2023-10-16 23:43:34 +0200

    Arrange project in folders

--- Project_SCP/Life/Player/Player.gd
+++ Project_SCP/Life/Player/Player.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 
-var MAX_SPEED = 100
+var ACCELERATION = 300
+var MAX_SPEED = 75
+var FRICTION = 350
+
+var input_vector = Vector2(0,0)
 
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
-	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	move_and_collide(velocity * delta * MAX_SPEED)
+	move_and_slide()
--- Project_SCP/Windows/Main_Menu/Menu.gd
+++ Project_SCP/Windows/Main_Menu/Menu.gd
@@ -6,4 +6,4 @@ func _on_button_pressed():
 
 
 func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://Level_test.tscn")
+	get_tree().change_scene_to_file("res://Levels/Level_test.tscn")
